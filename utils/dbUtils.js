8a63e1658a05ab29554364d04195ddba545afd1a
sugreev <ksugreev88@gmail.com>
2023-05-30 20:36:22 +0530

    update db timeout

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -4,7 +4,7 @@ const connectDb = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 15000,
     })
     .then(() => {
       console.log("Database connected successfully");
